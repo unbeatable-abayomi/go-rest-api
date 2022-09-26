edfaf2dd1e9f5945daea50e5ebaeec4540c3573f
abayomi igwubor <unbeatable-abayomi>
2022-09-26 07:37:20 +0100

    Added Health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	transportHTTP "unbeatable-abayomi/go-rest-api/internal/transport/http"
+)
 
 //App - the struct which contains things like pointers
 // to database connections
@@ -9,6 +13,13 @@ type App struct{}
 //Run - sets up our application
 func (app *App) Run() error{
 	fmt.Println("Setting up our App")
+	handler  := transportHTTP.NewHandler()
+
+	handler.SetupRoutes()
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil{
+		fmt.Println("Failed to set up server")
+		return err
+	}
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module unbeatable-abayomi/go-rest-api
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0
