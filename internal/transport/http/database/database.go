56a1cec96412358eff71f2cd5a5d41309e4eae23
abayomi igwubor <unbeatable-abayomi>
2022-10-06 11:09:57 +0100

    Added_DB_ConnectionString

--- internal/transport/http/database/database.go
+++ internal/transport/http/database/database.go
@@ -2,8 +2,8 @@ package database
 
 import (
 	"fmt"
-	"os"
-
+	_"os"
+    _"database/sql"
 	"github.com/jinzhu/gorm"
 	_"github.com/jinzhu/gorm/dialects/postgres"
 )
@@ -12,13 +12,22 @@ import (
 
 func NewDatabase()(*gorm.DB, error){
 	fmt.Println("Setting up new database connection")
-	dbUsername := os.Getenv("DB_USERNAME")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbTable := os.Getenv("DB_TABLE")
-	dbPort := os.Getenv("DB_PORT")
+	// dbUsername := os.Getenv("DB_USERNAME")
+	// dbPassword := os.Getenv("DB_PASSWORD")
+	// dbHost := os.Getenv("DB_HOST")
+	// dbTable := os.Getenv("DB_TABLE")
+	// dbPort := os.Getenv("DB_PORT")
 
-	connectString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", dbHost, dbPort, dbUsername, dbTable, dbPassword)
+	dbUsername := "postgres"
+	dbPassword := "abayomi"
+	dbHost := "localhost"
+	//dbName := "Restapi"
+	dbTable:= "Restapi"
+	dbPort := 5432
+
+
+
+	connectString := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable", dbHost, dbPort, dbUsername, dbTable, dbPassword)
 	db, err := gorm.Open("postgres", connectString)
 if err != nil {
  return db, err
@@ -30,3 +39,45 @@ if err := db.DB().Ping(); err != nil{
 
 	return db, nil
 }
+
+
+
+// func NewDatabase()(*sql.DB, error){
+// 	fmt.Println("Setting up new database connection")
+
+
+// 	dbUsername := "postgres"
+// 	dbPassword := "abayomi"
+// 	dbHost := "localhost"
+// 	dbName := "Restapi"
+// 	  dbTable:= "Comments"
+// 	dbPort := 5432
+// 	dbname := "CommentAPI"
+
+	
+
+// 	// connectString := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable", dbHost, dbPort, dbUsername, dbTable, dbPassword)
+// 	// db, err := gorm.Open("postgres", connectString)
+
+
+// 	url := fmt.Sprintf("host=%s port=%s user=%s password=%s sslmode=disable",dbHost, dbPort, dbUsername, dbPassword)
+//      db, err := sql.Open("postgres", url)
+// if err != nil {
+//     panic(err)
+// }
+// defer db.Close()
+
+// _, err = db.Exec(fmt.Sprintf("CREATE DATABASE %s;", dbname))
+// if err != nil {
+//     panic(err)
+// }
+
+// return db, err
+
+// //dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s sslmode=disable TimeZone=%s",)
+// //DB, _ := gorm.Open(postgres.Open(dsn))
+
+// //createDatabaseCommand := fmt.Sprintf("CREATE DATABASE %s", Config("DB_NAME"))
+// //DB.Exec(createDatabaseCommand)
+
+// }
